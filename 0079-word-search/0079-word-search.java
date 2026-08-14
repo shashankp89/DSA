87e845a9b981fedb2388138cd0522a08937c45f3
Shashank Pandey <pandeyshashank281@gmail.com>
2026-08-14 20:23:06 +0530

    Time: 129 ms (80.25%), Space: 42.7 MB (88.46%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean helper(char[][] board, String word, int i, int j, int idx) {
+        if (idx == word.length()) {
+            return true;
+        }
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(idx)) {
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = ' ';
+
+        boolean ans = helper(board, word, i + 1, j, idx + 1)
+                || helper(board, word, i - 1, j, idx + 1)
+                || helper(board, word, i, j + 1, idx + 1)
+                || helper(board, word, i, j - 1, idx + 1);
+
+        board[i][j] = temp;
+
+        return ans;
+    }
+
+    public boolean exist(char[][] board, String word) {
+        int m = board.length;
+        int n = board[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0)) {
+                    if (helper(board, word, i, j, 0)) {
+                        return true;
+                    }
+
+                }
+            }
+        }
+
+        return false;
+    }
+}
