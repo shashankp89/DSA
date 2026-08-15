8c896985df9adaa310893cf2bcb1b01e915fc5fb
Shashank Pandey <pandeyshashank281@gmail.com>
2026-08-15 11:34:10 +0530

    Time: 33 ms (71.67%), Space: 102.4 MB (23.33%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,29 @@
+class MinStack {
+    
+    Stack<int[]> st;
+
+    public MinStack() {
+        st = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if (st.isEmpty()) {
+            st.push(new int[]{val, val});
+        } else {
+            int currentMin = st.peek()[1];
+            st.push(new int[]{val, Math.min(val, currentMin)});
+        }
+    }
+    
+    public void pop() {
+        st.pop();
+    }
+    
+    public int top() {
+        return st.peek()[0];
+    }
+    
+    public int getMin() {
+        return st.peek()[1];
+    }
+}
