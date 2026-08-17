466a15e7c3cca32704e64ed670d031103f51451b
Shashank Pandey <pandeyshashank281@gmail.com>
2026-08-17 22:38:01 +0530

    Time: 0 ms (100%), Space: 48.3 MB (11.67%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,28 +1,26 @@
 class Solution {
-    public double calc(double x, long n){
-
-        if(n==0){
-            return (double)1;
-        }
-
-        
-        
-        if(n%2==0){
-            double ans=calc(x,n/2);
-            return  ans*ans;
-        }else{
-            double ans=calc(x,(n/2));
-            return x*ans*ans;
-        }
-
-    }
     public double myPow(double x, int n) {
-
-        if(n<0){
-            return (double)1/calc(x,-n);
-        }else{
-            return calc(x,n);
+          
+        long N = n;
+        
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
         }
         
+        double ans = 1.0;
+        
+        while (N > 0) {
+            
+            if ((N & 1) == 1) {
+                ans *= x;
+            }
+            
+            x *= x;
+            
+            N >>= 1;
+        }
+        
+        return ans;
     }
 }
