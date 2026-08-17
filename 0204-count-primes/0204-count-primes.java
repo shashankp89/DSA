9583c95d653217f20b0d82d41754277f4f595264
Shashank Pandey <pandeyshashank281@gmail.com>
2026-08-17 22:35:54 +0530

    Time: 322 ms (5.04%), Space: 46.6 MB (99.07%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,20 +1,22 @@
 class Solution {
     public int countPrimes(int n) {
         
-        if(n <= 2) return 0;
-         
+        if (n <= 2) return 0;
+ 
         int count = n / 2;
-        boolean[] isComposite = new boolean[n];
          
-        for(int i = 3; i * i < n; i += 2) {
-            
-            if(!isComposite[i]) {
-                 
-                for(int j = i * i; j < n; j += 2 * i) {
-                    
-                    if(!isComposite[j]) {
-                        isComposite[j] = true;
-                        count--;  
+        int[] composite = new int[(n >> 6) + 1];
+        
+        for (int i = 3; i * i < n; i += 2) {
+             
+            if ((composite[i >> 6] & (1 << ((i >> 1) & 31))) == 0) {
+                
+                for (int j = i * i; j < n; j += 2 * i) {
+                     
+                    if ((composite[j >> 6] & (1 << ((j >> 1) & 31))) == 0) {
+ 
+                        composite[j >> 6] |= (1 << ((j >> 1) & 31));
+                        count--;
                     }
                 }
             }
