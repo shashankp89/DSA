54135f3619ffedd56407fa05078a72095601f444
Shashank Pandey <pandeyshashank281@gmail.com>
2026-08-18 22:24:55 +0530

    Time: 2 ms (55.56%), Space: 87.6 MB (61.99%) - LeetHub

--- 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.java
+++ 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.java
@@ -3,32 +3,19 @@ class Solution {
         
         int n = a.length;
         int m = b.length;
-        int r = 0;
         
-        for(int i = 0; i < 32; i = i + 1){
-            int c1 = 0;
-            
-            for(int j = 0; j < n; j = j + 1){
-                if(((a[j] >> i) & 1) == 1){
-                    c1 = c1 + 1;
-                }
-            }
-            
-            int c2 = 0;
-            
-            for(int j = 0; j < m; j = j + 1){
-                if(((b[j] >> i) & 1) == 1){
-                    c2 = c2 + 1;
-                }
-            }
-            
-            if(c1 % 2 == 1){
-                if(c2 % 2 == 1){
-                    r = r | (1 << i);
-                }
-            }
+        int x = 0;
+        
+        for(int i = 0; i < n; i = i + 1){
+            x = x ^ a[i];
         }
         
-        return r;
+        int y = 0;
+        
+        for(int i = 0; i < m; i = i + 1){
+            y = y ^ b[i];
+        }
+        
+        return x & y;
     }
 }
