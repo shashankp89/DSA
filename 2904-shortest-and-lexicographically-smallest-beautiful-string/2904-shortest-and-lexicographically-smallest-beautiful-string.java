fd6014a6f9d85896635cdb2826127a64d498f879
Shashank Pandey <pandeyshashank281@gmail.com>
2026-08-26 09:18:24 +0530

    Time: 1 ms (100%), Space: 44 MB (75.84%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -2,27 +2,54 @@ class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
         
         int n = s.length();
-        String ans = "";
+        int l = 0;
+        int c = 0;
+        int bl = -1;
+        int br = n;
         
-        for(int i = 0; i < n; i = i + 1){
-            for(int j = i + 1; j <= n; j = j + 1){
-                String sub = s.substring(i, j);
-                int c = 0;
-                
-                for(int x = 0; x < sub.length(); x = x + 1){
-                    if(sub.charAt(x) == '1'){
-                        c = c + 1;
-                    }
+        for(int r = 0; r < n; r = r + 1){
+            if(s.charAt(r) == '1'){
+                c = c + 1;
+            }
+            
+            while(c > k || (l <= r && s.charAt(l) == '0')){
+                if(s.charAt(l) == '1'){
+                    c = c - 1;
                 }
+                l = l + 1;
+            }
+            
+            if(c == k){
+                int cl = r - l + 1;
+                int blen = br - bl + 1;
                 
-                if(c == k){
-                    if(ans.equals("") == true || sub.length() < ans.length() || (sub.length() == ans.length() && sub.compareTo(ans) < 0)){
-                        ans = sub;
+                if(bl == -1 || cl < blen){
+                    bl = l;
+                    br = r;
+                } else if(cl == blen){
+                    boolean f = false;
+                    
+                    for(int i = 0; i < cl; i = i + 1){
+                        if(s.charAt(l + i) < s.charAt(bl + i)){
+                            f = true;
+                            break;
+                        } else if(s.charAt(l + i) > s.charAt(bl + i)){
+                            break;
+                        }
+                    }
+                    
+                    if(f == true){
+                        bl = l;
+                        br = r;
                     }
                 }
             }
         }
         
-        return ans;
+        if(bl == -1){
+            return "";
+        }
+        
+        return s.substring(bl, br + 1);
     }
 }
