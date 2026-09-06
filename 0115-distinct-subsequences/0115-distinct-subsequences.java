febdeebc544837e5a9eac0d7f1cdea55115374dc
Shashank Pandey <pandeyshashank281@gmail.com>
2026-09-06 08:35:29 +0530

    Time: 14 ms (92.72%), Space: 42.8 MB (95.03%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,27 +3,21 @@ class Solution {
         int m = s.length();
         int n = t.length();
          
-        int[][] dp = new int[m + 1][n + 1];
+        int[] dp = new int[n + 1];
         
-        
-        for (int i = 0; i <= m; i++) {
-            dp[i][0] = 1;
-        }
-         
+       
+        dp[0] = 1; 
         
         for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                
+            
+            for (int j = n; j >= 1; j--) {
                 if (s.charAt(i - 1) == t.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-                } 
-               
-                else {
-                    dp[i][j] = dp[i - 1][j];
+                    dp[j] = dp[j] + dp[j - 1];
                 }
+                
             }
         }
         
-        return dp[m][n];
+        return dp[n];
     }
 }
