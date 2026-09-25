39566a8748b4ba7830838fdfd7cd67f05bb6a0a5
Shashank Pandey <pandeyshashank281@gmail.com>
2026-09-25 07:53:43 +0530

    Time: 10 ms (68.8%), Space: 47.2 MB (64%) - LeetHub

--- 1096-brace-expansion-ii/1096-brace-expansion-ii.java
+++ 1096-brace-expansion-ii/1096-brace-expansion-ii.java
@@ -1,36 +1,55 @@
 import java.util.*;
 
 class Solution {
+    private int index = 0;
+
     public List<String> braceExpansionII(String expression) {
-        Queue<String> queue = new LinkedList<>();
-        Set<String> resultSet = new HashSet<>();
-        queue.offer(expression);
+        Set<String> resultSet = parseExpression(expression);
+        List<String> result = new ArrayList<>(resultSet);
+        Collections.sort(result);
+        return result;
+    }
+    
+    private Set<String> parseExpression(String s) {
+        Set<String> currentSet = new HashSet<>();
+        Set<String> groupSet = new HashSet<>();
+        groupSet.add("");
         
-        while (!queue.isEmpty()) {
-            String curr = queue.poll();
+        while (index < s.length()) {
+            char c = s.charAt(index);
             
-            if (!curr.contains("{")) {
-                resultSet.add(curr);
-                continue;
-            }
-            
-            int right = curr.indexOf('}');
-            int left = right;
-            while (curr.charAt(left) != '{') {
-                left--;
-            }
-            
-            String before = curr.substring(0, left);
-            String after = curr.substring(right + 1);
-            String[] parts = curr.substring(left + 1, right).split(",");
-            
-            for (String part : parts) {
-                queue.offer(before + part + after);
+            if (c == '{') {
+                index++;
+                Set<String> subSet = parseExpression(s);
+                groupSet = combine(groupSet, subSet);
+            } else if (c == '}') {
+                index++;
+                currentSet.addAll(groupSet);
+                return currentSet;
+            } else if (c == ',') {
+                index++;
+                currentSet.addAll(groupSet);
+                groupSet = new HashSet<>();
+                groupSet.add("");
+            } else {
+                Set<String> charSet = new HashSet<>();
+                charSet.add(String.valueOf(c));
+                groupSet = combine(groupSet, charSet);
+                index++;
             }
         }
         
-        List<String> result = new ArrayList<>(resultSet);
-        Collections.sort(result);
+        currentSet.addAll(groupSet);
+        return currentSet;
+    }
+    
+    private Set<String> combine(Set<String> set1, Set<String> set2) {
+        Set<String> result = new HashSet<>();
+        for (String s1 : set1) {
+            for (String s2 : set2) {
+                result.add(s1 + s2);
+            }
+        }
         return result;
     }
 }
